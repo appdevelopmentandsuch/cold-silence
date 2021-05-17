2e503dd39eb412708deebf779827cc6842df885a
Paul Bock <bocklucas@yahoo.ca>
2021-05-17 09:14:12 -0500

    Adding more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -57,3 +57,56 @@ class MainTestSuite(unittest.TestCase):
         self.assertEqual(
             True, os.path.exists(os.path.dirname(DEFAULT_PATH + "/prod.env"))
         )
+
+    def test_create_verbose_project(self):
+
+        generate_project(verbose=True)
+
+        self.assertEqual(
+            True,
+            os.path.exists(
+                os.path.dirname(
+                    DEFAULT_PATH + "/" + DEFAULT_PROJECT_DIRECTORY + "/" + "Dockerfile"
+                )
+            ),
+        )
+        self.assertEqual(
+            True,
+            os.path.exists(
+                os.path.dirname(
+                    DEFAULT_PATH
+                    + "/"
+                    + DEFAULT_PROJECT_DIRECTORY
+                    + "/"
+                    + "Dockerfile.prod"
+                )
+            ),
+        )
+        self.assertEqual(
+            True,
+            os.path.exists(os.path.dirname(DEFAULT_PATH + "/" + "docker-compose.yaml")),
+        )
+        self.assertEqual(
+            True, os.path.exists(os.path.dirname(DEFAULT_PATH + "/nginx/Dockerfile"))
+        )
+        self.assertEqual(
+            True, os.path.exists(os.path.dirname(DEFAULT_PATH + "/nginx/nginx.conf"))
+        )
+        self.assertEqual(
+            True, os.path.exists(os.path.dirname(DEFAULT_PATH + "/settings.py"))
+        )
+        self.assertEqual(
+            True, os.path.exists(os.path.dirname(DEFAULT_PATH + "/.gitignore"))
+        )
+        self.assertEqual(
+            True, os.path.exists(os.path.dirname(DEFAULT_PATH + "/local.env"))
+        )
+        self.assertEqual(
+            True, os.path.exists(os.path.dirname(DEFAULT_PATH + "/dev.env"))
+        )
+        self.assertEqual(
+            True, os.path.exists(os.path.dirname(DEFAULT_PATH + "/staging.env"))
+        )
+        self.assertEqual(
+            True, os.path.exists(os.path.dirname(DEFAULT_PATH + "/prod.env"))
+        )
